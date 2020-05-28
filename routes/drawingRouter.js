23793bb7bbf523e142e4b719bcc5b0ba41270ea9
Vruddhi Mehta <vgmehta_b17@ce.vjti.ac.in>
2020-05-28 19:59:59 +0530

    Removed Commented Code

--- routes/drawingRouter.js
+++ routes/drawingRouter.js
@@ -8,32 +8,8 @@ drawingRouter.use(bodyParser.json());
 drawingRouter.route('/:room_id')
     .put((req,res,next) => {
         let roomId = req.params.room_id;
-        // let json = req.body.canvas_json;
         let base64 = req.body.base64;
         if (base64 != null) {
-            // if (json != '') {
-            //     req.redis.hmset(roomId, {
-            //         'canvas_json' : JSON.stringify(json),
-            //         'base64' : base64
-            //     }, (err,reply) => {
-            //         if (!reply) {
-            //             res.send(err);
-            //         } else {
-            //             res.send(reply);
-            //         }
-            //     });
-            // } else {
-            //     req.redis.hmset(roomId, {
-            //         'canvas_json' : json,
-            //         'base64' : base64
-            //     }, (err,reply) => {
-            //         if (!reply) {
-            //             res.send(err);
-            //         } else {
-            //             res.send(reply);
-            //         }
-            //     });
-            // }
             req.redis.hmset(roomId, {
                 'base64' : base64
             }, (err,reply) => {
