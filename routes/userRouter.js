6f69068e7849150b34deeff172f8ad0eb81c1a88
Ketan Khanolkar <khanolkarketan645@gmail.com>
2020-05-31 14:40:25 +0530

    removed logs

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -9,13 +9,11 @@ userRouter.route('/:userId')
     .get((req,res,next) => {
         let userId = req.params.userId;
         req.redis.hget('users', userId, function(err, userList){
-            console.log(userList);
             if (!userList) {
                 res.send('User does not exist');
             } else {
                 res.send(userList);
             }
-
         });
     }); 
     
