833b4f4e962abc8d82036ee1c4f24264db3cf6dc
vgmehta <48943358+vgmehta@users.noreply.github.com>
2020-06-05 11:29:15 +0530

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const userRouter = require('./routes/userRouter');
 const boardRouter = require('./routes/boardRouter');
 const roomRouter = require('./routes/roomRouter');
 
-const hostname = '0.0.0.0';
+const hostname = 'http://digi-planner-tool-digi-planner-tool.apps.123.252.203.195.nip.io';
 const port = 8080;
 
 const app = express();
@@ -69,28 +69,28 @@ router.get('/*', (req, res, next) => {
   res.sendFile(path.join(__dirname, './public/dist/digi-planner/index.html'));
 });
 
-// client.hmset("users", {
-//     'vruddhigmehta@gmail.com': '1',
-//     'seno.29.11.gupta@gmail.com': '0',
-//     'vgmehta@ce.vjti.ac.in': '0',
-//     'kiranambokar6@gmail.com': '1',
-//     'kdambokar_b17@it.vjti.ac.in': '0',
-//     'bhaleraoshubham99@gmail.com': '1',
-//     'khanolkarketan@gmail.com': '1',
-//     'asrarul97@gmail.com': '1',
-//     'dikshagupta2012@gmail.com': '1',
-//     'helloworldmanit@gmail.com': '0',
-//     'vedantmathe@gmail.com': '1',
-//     'sdbhalerao_b17@ce.vjti.ac.in': '0',
-//     'mathevedant@gmail.com': '0',
-//     'kits41999@gmail.com': '0'
-//   }, (err, reply) => {
-//     if (!reply) {
-//       console.log(err);
-//     } else {
-//       console.log(reply);
-//     }
-//   });
+client.hmset("users", {
+    'vruddhigmehta@gmail.com': '1',
+    'seno.29.11.gupta@gmail.com': '0',
+    'vgmehta@ce.vjti.ac.in': '0',
+    'kiranambokar6@gmail.com': '1',
+    'kdambokar_b17@it.vjti.ac.in': '0',
+    'bhaleraoshubham99@gmail.com': '1',
+    'khanolkarketan@gmail.com': '1',
+    'asrarul97@gmail.com': '1',
+    'dikshagupta2012@gmail.com': '1',
+    'helloworldmanit@gmail.com': '0',
+    'vedantmathe@gmail.com': '1',
+    'sdbhalerao_b17@ce.vjti.ac.in': '0',
+    'mathevedant@gmail.com': '0',
+    'kits41999@gmail.com': '0'
+  }, (err, reply) => {
+    if (!reply) {
+      console.log(err);
+    } else {
+      console.log(reply);
+    }
+  });
 //Socket Part Added
 io.on("connection", (socket) => {
   console.log("connection added");
